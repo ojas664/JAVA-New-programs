a64854bd2ed9ba875049251fa14b0f8f9035a56c
Ojas Gupta <ojas774@gmail.com>
2023-03-17 22:22:58 +0530

    new programs

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String name="ojas and ishu";
+        String name1="gupta";
+        System.out.println(name+" and "+name1);
+        System.out.println(name.length());
+        System.out.println(name1.length());
+        System.out.println(name.charAt(3));
+        String name2= name1.replace('a','w');
+        System.out.println(name.substring(9,13));
+        System.out.println(name.substring(5,8));
+
+
+
+
+    }
+}
