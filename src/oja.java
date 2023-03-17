8421c844062b5c7bb77735750ffcf87117171cc1
Ojas Gupta <ojas774@gmail.com>
2023-03-17 22:18:04 +0530

    new programs

---
+++ src/oja.java
@@ -0,0 +1,6 @@
+public class oja{
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/problems.java
@@ -0,0 +1,38 @@
+public class problems {
+    public static void main(String[] args) {
+
+        // write java programe to print hellow world and your name on next line
+
+        System.out.println("hellow world");
+        System.out.println("hi i am ojas");
+
+        // write a java programe to print sum of two numbers data is (a=74 & b=36)
+
+        int a= 74;
+        int b=36;
+        int r= a+b;
+        System.out.println("addition is:"+r);
+
+        // write a java programe to divide  two numbers data is (a=74 & b=2)
+
+        int x= 74;
+        int y=2;
+        int z= x/y;
+        System.out.println("division is :" + z);
+
+        // write a java programe to print the result of the following operations:
+        /* -5+8*6
+           (55+9)%9
+           20+-3*5/8
+           5+15/3*2-8%3
+         */
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(20+-3*5/8);
+        System.out.println(5+15/3*2-8%3);
+
+
+    }
+}
+
+
