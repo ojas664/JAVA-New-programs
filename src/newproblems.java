e34e2dff825fc7e4391f7152efa7920e15617f6e
Ojas Gupta <ojas774@gmail.com>
2023-03-22 09:13:45 +0530

    new programs

---
+++ src/newproblems.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class newproblems{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int mul = a*b;
+        System.out.println("first value is: "+a);
+        System.out.println("second value is: "+b);
+        System.out.println("multiplication is: "+mul);
+    }
+}
