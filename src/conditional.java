6bbe2fb9f7322f5d71234d77a827a86a1e9cb708
Ojas Gupta <ojas774@gmail.com>
2023-03-26 01:07:05 +0530

    new programs

---
+++ src/conditional.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class conditional{
+    public static void main(String[] args) {
+       // WRITE PROGRAM TO FIND EVEN AND ODD NUMBERS :
+
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age%2==0) {
+            System.out.println("EVEN NUMBER..!! ");
+        }
+        else {
+            System.out.println("ODD NUMBER..!!");
+        }
+
+    }
+}
