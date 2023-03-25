5d64ce9eaaeecde8e3f5a73b770abfc780cfddbc
Ojas Gupta <ojas774@gmail.com>
2023-03-26 01:40:14 +0530

    WRITE A PROGRAM TO CHECK EVEN ODD NUMBERS..!!

---
+++ src/conditionalstat.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class conditionalstat {
+    public static void main(String[] args)
+    {
+        // WRITE A PROGRAM TO CHECK EVEN ODD NUMBERS..!!
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 2 == 0)
+        {
+            System.out.println("EVEN NUMBER:");
+        }
+        else
+        {
+            System.out.println("ODD NUMBER :");
+        }
+    }
+}
