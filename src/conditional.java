9219511193502c81512bf6f866a8d178805a631c
Ojas Gupta <ojas774@gmail.com>
2023-03-26 01:28:21 +0530

    WRITE A PROGRAM TO CHECK CONDITION:

--- src/conditional.java
+++ src/conditional.java
@@ -1,16 +1,26 @@
 import java.util.*;
 class conditional{
-    public static void main(String[] args) {
-       // WRITE PROGRAM TO FIND EVEN AND ODD NUMBERS :
+    public static void main(String[] args)
+    {
+        // WRITE A PROGRAM TO CHECK CONDITION:
+        // a==b print ( a is equal to b)
+        // a>b print ( a is greater than b)
+        // a<b print ( b is greater than a)
 
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if(age%2==0) {
-            System.out.println("EVEN NUMBER..!! ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a==b)
+        {
+            System.out.println("a is equal to b");
         }
-        else {
-            System.out.println("ODD NUMBER..!!");
+        else if(a>b)
+        {
+            System.out.println("a is greater than b");
         }
-
+        else
+        {
+            System.out.println("b is greater than a");
+        }
     }
 }
